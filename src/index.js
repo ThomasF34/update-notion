d12ceb4f1cbf57feab370e68a6079df0eed574aa
phakawatTER <phakawatTER@github.com>
2023-03-17 15:22:42 +0700

    log data

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const params = extractParams()
 const enhancedRegex = `${params.prefix}${URL_REGEX}${params.suffix}`
 
 const urls = params.pullRequest.body.match(enhancedRegex) ?? []
-console.debug(params.pullRequest.body)
+core.debug(params.pullRequest.body)
 const urlFound = urls.find((url) => url.match("notion.so"))
 
 if (urlFound) {
@@ -28,7 +28,7 @@ if (urlFound) {
     })
     .find((url) => url.match("notion.so"))
     .split("/")
-  console.debug(notionUrlParts)
+  core.debug(notionUrlParts)
   const taskName = notionUrlParts[notionUrlParts.length - 1]
 
   const taskParts = taskName.split("-")
